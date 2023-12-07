d2f061cf03ea8f068558e45b2f2437e843537de3
Amine <thenono1111@gmail.com>
2023-12-07 20:53:34 +0100

    echo works

--- srcs/builtins/mini_echo.c
+++ srcs/builtins/mini_echo.c
@@ -6,47 +6,48 @@
 /*   By: ael-malt <ael-malt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:40:59 by ael-malt          #+#    #+#             */
-/*   Updated: 2023/12/07 20:29:29 by ael-malt         ###   ########.fr       */
+/*   Updated: 2023/12/07 20:52:13 by ael-malt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-// static void	print_echo(t_astn *tree, int i)
-// {
-// 	while (tree->token && tree->token[i])
-// 	{
-// 		ft_printf("%s", tree->token[i]->content);
-// 		if (tree->token[i + 1]->content)
-// 			ft_printf(" ");
-// 		i++;
-// 	}
-// }
+static void	print_echo(t_astn *tree, int i)
+{
+	while (tree->token && tree->token[i] && tree->token[i]->content)
+	{
+		ft_printf("%s", tree->token[i]->content);
+		if (tree->token[i + 1] && tree->token[i + 1]->content)
+			ft_printf(" ");
+		i++;
+	}
+}
 
 int	mini_echo(t_astn *tree)
 {
-	// int	i;
-	// int	n;
-	// int	j;
+	int	i;
+	int	n;
+	int	j;
 
-	// i = 1;
-	// n = 0;
-		printf("%s\n", tree->token[0]->content);
-	// while (tree->token[i]->content && !ft_strncmp(tree->token[i]->content, "-n", 2))
-	// {
-		// j = 1;
-		// while (tree->token[i]->content[j] == 'n')
-		// 	j++;
-		// if (tree->token[i]->content[j - 1] && tree->token[i]->content[j] == '\0')
-		// {
-		// 	n = 1;
-			// i++;
-		// }
-		// else
-		// 	break ;
-	// }
-	// print_echo(tree, i);
-	// if (!n)
-	// 	ft_printf("\n");
+	i = 1;
+	n = 0;
+	while (tree->token[i] && tree->token[i]->content && \
+		!ft_strncmp(tree->token[i]->content, "-n", 2))
+	{
+		j = 1;
+		while (tree->token[i] && tree->token[i]->content[j] == 'n')
+			j++;
+		if (tree->token[i] && tree->token[i]->content[j - 1]
+			&& tree->token[i]->content[j] == '\0')
+		{
+			n = 1;
+			i++;
+		}
+		else
+			break ;
+	}
+	print_echo(tree, i);
+	if (!n)
+		ft_printf("\n");
 	return (0);
 }
--- srcs/main/minishell.c
+++ srcs/main/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ael-malt <ael-malt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 10:37:38 by rmohamma          #+#    #+#             */
-/*   Updated: 2023/12/07 20:30:48 by ael-malt         ###   ########.fr       */
+/*   Updated: 2023/12/07 20:47:11 by ael-malt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -133,10 +133,7 @@ int	shell_loop(t_astn *tree, t_env *sh_env, t_cleanup *cl)
 	else if (get_herd(tree->token, &pos))
 		exe_herd(tree, pos, sh_env, cl);
 	else if (tree->token[0]->type && !(tree->token[0]->type % 11))
-	{
-		printf("ici\n");
 		builtin(tree, cl);
-	}
 	else
 		execute(tree, sh_env, cl);
 	if (tree == cl->tree)
